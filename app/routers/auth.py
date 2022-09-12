d0f7fc5537a5f51a7a144b45aec0275a529b1957
shangchenhsieh <seankh4444@gmail.com>
2022-09-12 11:49:34 -0700

    Sep 12

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
-
 router = APIRouter(tags=['Authentication'])
+
 
 @router.post("/login", response_model=schemas.Token)
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
@@ -19,6 +19,5 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
 
     # create a token
     # return a token
-    access_token = oauth2.create_access_token(data={"user_id": user.id })
-    return {"access_token": access_token, "token_type": "Bearer"}
-
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
