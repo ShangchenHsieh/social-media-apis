433e457c0c519026ae3f2416091e4dddaf92f878
shangchenhsieh <seankh4444@gmail.com>
2022-09-20 20:35:32 -0700

    Sep 20

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from . import models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 from .config import settings
 
 
@@ -20,7 +20,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
