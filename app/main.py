68eef3c86dd1df2a01c8ea3d72b575e208ee0416
shangchenhsieh <seankh4444@gmail.com>
2022-10-03 10:46:16 -0700

    Oct3

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import psycopg2
 from fastapi import FastAPI, Response, HTTPException, Depends
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseSettings
+from starlette.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
@@ -9,18 +11,25 @@ from typing import List
 from .routers import post, user, auth, vote
 from .config import settings
 
-
-
-
-#  models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
 
 @app.get("/")
 def root():
