b6e0e8cfa3f3f60d435fe1977f9e573e2c9437c2
shangchenhsieh <seankh4444@gmail.com>
2022-10-14 16:17:20 -0700

    Oct 14

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
-from .database import Base
+
+
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+from .database import Base
 
 
 class Post(Base):
