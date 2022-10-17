3734f899566abe2a97ecf529d7f3cf1ad535832b
shangchenhsieh <seankh4444@gmail.com>
2022-10-16 23:00:21 -0700

    Oct16

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from .database import engine, get_db
 from .routers import post, user, auth, vote
 from .config import settings
 
-get_db()
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
